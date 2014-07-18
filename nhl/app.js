cb98b0d090b3e3c52f7a18becd8d7b08917a19ff
David Leonard <sephirothcloud1025@yahoo.com>
2014-07-18 16:31:01 -0400

    Instaling requests library for node

--- nhl/app.js
+++ nhl/app.js
@@ -4,6 +4,7 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var request = require("request");
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
