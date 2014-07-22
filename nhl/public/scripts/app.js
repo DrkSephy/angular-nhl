509f3adee4e03c948077290e739b9471747ae0c0
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-21 23:48:42 -0400

    Added simple controller and view.

--- nhl/public/scripts/app.js
+++ nhl/public/scripts/app.js
@@ -3,4 +3,14 @@
 var nhlApp = angular.module('nhlApp', [
     'ui.router'
 ])
-.config(function($stateProvider, $urlRouterProvider) {});
+.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('hello', {
+            url: '/hello',
+            templateUrl: 'partials/hello.partial.html',
+            controller: 'helloController',
+            data: {
+                pageTitle: 'Hello'
+            }
+        });
+});
---
+++ nhl/public/scripts/controllers/helloController.js
@@ -0,0 +1,5 @@
+'use strict';
+
+nhlApp.controller('helloController', function($scope) {
+    $scope.pageData = 'Hello World';
+});
