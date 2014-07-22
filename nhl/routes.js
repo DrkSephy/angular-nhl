9511fe745230ed5fd415aca5927aad40f89c2804
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-21 23:17:48 -0400

    Cleaned up app.js. Renamed app.js to server.js. Moved Express routes to routes.js.

---
+++ nhl/routes.js
@@ -0,0 +1,7 @@
+module.exports = function(router) {
+    // Router will handle any requests with this endpoint depending on where router is "use()'d.
+    //router.get('/_____', function(req, res) {
+    //    // Returns a JSON response when user visits this endpoint
+    //    res.json({});
+    //});
+}
---
+++ nhl/server.js
@@ -0,0 +1,22 @@
+// Require needed modules
+var express = require('express');
+
+// Create an Express application
+var app = express();
+
+// Get an instance of the Express Router
+var router = express.Router();
+
+// Register router routes
+require('./routes')(router);
+
+// Prefix routes with /api
+app.use('/api', router);
+
+// Set the static files location
+app.use(express.static('./public'));
+
+// Start application server
+app.listen(8080, '127.0.0.1', function() {
+    console.log('Express server started on 127.0.0.1:8080');
+});
